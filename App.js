5390a01ff73ecae7e928fb4f5f1ac99a63ad13c6
Dev_ As_ <d.e.v.a.s.1.7.0.9.5.6@gmail.com>
2024-02-20 19:30:04 +0530

    JSX intro , Transpiling , Parcel - Babel Js Engine

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// React.createElement => Object => HTMLElement(render)
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "React H1"),
-    React.createElement("h2", {}, "React H2"),
-  ])
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React 🚀"
 );
 
-console.log(parent);
+console.log(heading);
+
+// JSX (Transpiled before it reaches the JS) - Parcel - Babel
+
+const jsxHeading = <h1>Nmaste React By JSX 🚀</h1>; //Conver this into browser understand language that is Ecmascript pure Javascript
+
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+//ReactDom will conver react element into HTML
+
+root.render(jsxHeading);
