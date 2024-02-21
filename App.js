610a17a740d16949a46e538ab38c63d0a77622f1
Dev_ As_ <d.e.v.a.s.1.7.0.9.5.6@gmail.com>
2024-02-21 09:29:28 +0530

    react component creation,what is react component,syntax

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => ReactElementJS-Object => HTMLElement(render)
-
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Namaste React 🚀"
 );
-
-console.log(heading);
-
-// JSX (Transpiled before it reaches the JS) - Parcel - Babel
-
-// JSX => React.createElement => ReactElementJS-Object => HTMLElement(render)
-
 const jsxHeading = (
   <>
     <h1 className="heading" tabIndex="1">
@@ -22,12 +13,34 @@ const jsxHeading = (
     </h1>
     <p>Paragraph</p>
   </>
-); //Conver this into browser understand language that is Ecmascript pure Javascript
+);
 
-console.log(jsxHeading);
+// React Component
+// Class Based Component
+// Functional Component
+
+// React Functional Component
+
+// This is how we create  REACT COIMPONENT USING return keyword
+const HeaderComponent = () => {
+  return (
+    <h1 className="heading" tabIndex="1">
+      Namaste React By JSX 🚀
+    </h1>
+  );
+};
+
+//this is how we create React component just wraping it into a () paranthisis not using return keyword
+
+const Header2Component = () => (
+  <h1 className="heading" tabIndex="1">
+    Namaste React By JSX 🚀
+  </h1>
+);
+
+//Both way to create react component is valid syntax
+// React component is just a javascript function that return some Jsx or React element
+// And the first letter of react component name is must be Capital letter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//ReactDom will conver react element into HTML
-
 root.render(jsxHeading);
