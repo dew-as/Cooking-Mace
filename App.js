f7cc8184283f4169c03b701f6861dd7e61414af1
Dev_ As_ <d.e.v.a.s.1.7.0.9.5.6@gmail.com>
2024-02-21 13:47:21 +0530

    what are the ways to execute react component inside

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//this is an api call for attack us, so if the code is exicuted that will be affect our safety
-const malisiousData = api.getData("malisious data")
+const FooterComponent = () => (
+  <div id="footer">
+    <h1 className="heading" tabIndex="1">
+      Namaste Javascript Footer
+    </h1>
+  </div>
+);
 
 const HeaderComponent = () => (
-  <div id="container">
-    {malisiousData} { /* dont be afraid JSX will take care of it , JSX is not executing tha data directly , JSX  will be automatically sanitize the data before execution,so no one can attack us */ }
+  <div id="header">
+    <FooterComponent /> {/* This is the way to call the react component */}
+
+    <FooterComponent></FooterComponent> {/* this is another way to call this have another use case */}
+
+    {FooterComponent()} {/* at the end react component means functinal component is a normal javascript function , and inside that the jsx will be converted into javascript by "babel" so we have now a function with an react element that is a javascript object ,it means a function assigned with the value is a object so we can also call the function in jsx how we call we need { } right */}
+
+    {/* these all are same in result */}
+
     <h1 className="heading" tabIndex="1">
-      Namaste Javascript
+      Namaste Javascript Header
     </h1>
   </div>
 );
