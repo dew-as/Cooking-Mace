7b72cea3c8bd717bb2c006cfe051d6cd533b0243
Dev_ As_ <d.e.v.a.s.1.7.0.9.5.6@gmail.com>
2024-02-21 13:31:28 +0530

    what about safety when using JSX

--- App.js
+++ App.js
@@ -1,45 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = <h1>i am element to element</h1>;
-
-const HeadComponent = () => (
-  <div>
-    <h1 className="heading" tabIndex="1">
-      i am component to element
-    </h1>
-  </div>
-);
-
-const heading2 = (
-  <div>
-    <h2>i am element to component</h2>
-    <HeadComponent />
-    {heading1}
-  </div>
-);
-
-const ReactComponent = () => (
-  <div>
-    <h1 className="heading" tabIndex="1">
-      i am component to component
-    </h1>
-    {heading2}
-  </div>
-);
+//this is an api call for attack us, so if the code is exicuted that will be affect our safety
+const malisiousData = api.getData("malisious data")
 
 const HeaderComponent = () => (
   <div id="container">
-    <ReactComponent />
+    {malisiousData} { /* dont be afraid JSX will take care of it , JSX is not executing tha data directly , JSX  will be automatically sanitize the data before execution,so no one can attack us */ }
     <h1 className="heading" tabIndex="1">
-      Basically these all are javascript.... react element becomes js object ....
-      jsx becomes react element and then js object....component is used to write JSX and
-      we can write js by adding {"{ }"} in component we can write any javascript code 🚀
+      Namaste Javascript
     </h1>
   </div>
 );
-
-// if we use a component inside another component like this known as Component Composition 🚀
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
