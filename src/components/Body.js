4c9c8f288d69e33649310a273c34a91da554aeba
Dev_ As_ <d.e.v.a.s.1.7.0.9.5.6@gmail.com>
2024-02-24 22:23:48 +0530

    lets get hooked- virtual dom and react fiber knowledge

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
 import { data } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  // const [listOffRestaurants, setListOffRestaurants] = useState(data);
+
+  const arr = useState(data);
+  // const [listOffRestaurants, setListOffRestaurants] = arr
+
+  const listOffRestaurants = arr[0];
+  const setListOffRestaurants = arr[1];
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOffRestaurants.filter(
+              (restaurant) => restaurant.info.avgRating > 4
+            );
+            setListOffRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {data.map((restaurant) => (
+        {listOffRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
