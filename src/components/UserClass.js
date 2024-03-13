14f890e486f58585ae8bc34dc656d8de51ffac87
Dev_ As_ <d.e.v.a.s.1.7.0.9.5.6@gmail.com>
2024-03-13 11:51:18 +0530

    how we can create a state variable inside a class component, when we create an instance of class that means cunstructor,this.state is an objects contains all state variables,use the state by this.state || and destructure

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,12 +4,19 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
+    this.state = {
+      count: 0,
+    };
   }
 
   render() {
     const { name, location, contact } = this.props;
+    const { count } = this.state;
     return (
       <div className="user-class">
+        <span>Count : {this.state.count}</span>
+        <hr/>
+        <span>Count : {count}</span>
         <h1>{name}</h1>
         <h2>{location}</h2>
         <h3>{contact}</h3>
