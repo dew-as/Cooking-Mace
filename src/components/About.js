2b3d2a8fbd9f3549103de0526ea928cf163522bb
Dev_ As_ <d.e.v.a.s.1.7.0.9.5.6@gmail.com>
2024-03-14 10:35:33 +0530

    Github user API call,data and life cycle

--- src/components/About.js
+++ src/components/About.js
@@ -4,33 +4,19 @@ import UserClass from "./UserClass";
 class About extends Component {
   constructor(props) {
     super(props);
-    console.log("Parent constructor");
+    // console.log("Parent constructor");
   }
 
   componentDidMount() {
-    console.log("Parent componentDidMount");
+    // console.log("Parent componentDidMount");
   }
 
   render() {
-    console.log("Parent render");
+    // console.log("Parent render");
     return (
       <div className="about">
         <h1>User Details</h1>
-        <UserClass
-          name={"First Child"}
-          location={"Kerala"}
-          contact={"dewasdevelops@gmail.com"}
-        />
-        <UserClass
-          name={"Second Child"}
-          location={"Kerala"}
-          contact={"dewasdevelops@gmail.com"}
-        />
-        <UserClass
-          name={"Third Child"}
-          location={"Kerala"}
-          contact={"dewasdevelops@gmail.com"}
-        />
+        <UserClass />
       </div>
     );
   }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,47 +4,68 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "user",
+        location: "user location",
+        avatar_url:
+          "https://img.freepik.com/premium-photo/futuristic-warrior-avtar_862994-26466.jpg",
+      },
     };
-    console.log(this.props.name + "constructor");
+    console.log("constructor");
   }
 
-  componentDidMount() {
-    console.log(this.props.name + "componentDidMount");
+  async componentDidMount() {
+    console.log("componentDidMount");
+    const data = await fetch("https://api.github.com/users/dew-as");
+    const json = await data.json();
+
+    console.log(json);
+
+    this.setState({
+      userInfo: json,
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("Component did update");
   }
 
   render() {
-    console.log(this.props.name + "render");
-    const { name, location, contact } = this.props;
-    const { count } = this.state;
+    console.log("render");
+    const { name, location, avatar_url, blog, email, url } =
+      this.state.userInfo;
     return (
       <div className="user-class">
-        <span>Count : {count}</span>
-        <hr />
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count + 1,
-            });
-          }}
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => {
-            this.setState({
-              count: this.state.count - 1,
-            });
-          }}
-        >
-          Decrement
-        </button>
-        <h1>{name}</h1>
+        <div>
+          <img src={avatar_url} alt="" />
+          <h1>{name}</h1>
+        </div>
         <h2>{location}</h2>
-        <h3>{contact}</h3>
+        <a style={{ textDecoration: "none" }} href={blog || url}>
+          Site
+        </a>
       </div>
     );
   }
 }
 
 export default UserClass;
+
+/**
+ *  --- Mounting ---
+ *
+ *  Constructor (dummy data)
+ *  Render (Dummy data)
+ *       <HTML Dummy >
+ *  Component did mount
+ *       <Api Call>
+ *       this.setState -> State Variable is updated
+ *
+ *
+ *  --- Update ---
+ *    render (API DATA)
+ *    <HTML Api data.
+ *  ComponentDidUpdate
+ *
+ *
+ */
