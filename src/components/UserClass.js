22d838f9443dee4ffe0d750457373f3194a56234
Dev_ As_ <d.e.v.a.s.1.7.0.9.5.6@gmail.com>
2024-03-14 12:01:51 +0530

    how life cycle methods are diff from use effect,how we can do samethings in class based components like state change dependancy

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -26,11 +26,28 @@ class UserClass extends React.Component {
     });
   }
 
-  componentDidUpdate() {
+  // if we want to do anything on specific state changes how will be modify our componentDidUpdate ?
+
+  // we need to check that whether the previous state and current state changed or not in componentDidUpdate.
+
+  // if we want to do something when multiple state changes that means same thing to do then we have to put || in the condition like
+
+  // if(prevState !== this.state.userInfo || this.state.userData ) // Do something
+
+  // if we want to do something when multiple state changes that means differents thing to do then we have to put more conditions like
+
+  // if(prevState !== this.state.userInfo) // Do something
+
+  // if(prevState !== this.state.userData) // Do something
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState !== this.state.userInfo) {
+      // Do something
+    }
     console.log("Component did update");
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     console.log("This will log when the component will unmount");
   }
 
